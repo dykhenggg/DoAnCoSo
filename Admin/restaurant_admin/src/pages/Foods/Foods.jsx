a75340ac374e7310b5bf480477876a3c04cfe178
anhvu256 <ngoanhvu256@gmail.com>
2025-05-23 02:42:47 +0700

    merge succeed

--- Admin/restaurant_admin/src/pages/Foods/Foods.jsx
+++ Admin/restaurant_admin/src/pages/Foods/Foods.jsx
@@ -3,7 +3,7 @@ import "./Foods.css";
 import axios from "axios";
 import { toast } from "react-toastify";
 
-const Foods = () => {
+function Foods() {
   const [foods, setFoods] = useState([]);
   const [categories, setCategories] = useState([]);
   const [showAddModal, setShowAddModal] = useState(false);
@@ -111,23 +111,7 @@ const Foods = () => {
                 </td>
                 <td>{food.tenMon}</td>
                 <td>{food.gia.toLocaleString()} VNĐ</td>
-                // Trong phần hiển thị bảng, thay đổi cột Loại món
                 <td>{food.loaiMon?.tenLoai}</td>
-                // Trong phần modal thêm món ăn, sửa select box như sau:
-                <select
-                  value={newFood.loaiMon}
-                  onChange={(e) =>
-                    setNewFood({ ...newFood, loaiMon: e.target.value })
-                  }
-                  required
-                >
-                  <option value="">Chọn loại món</option>
-                  {categories.map((category) => (
-                    <option key={category.maLoai} value={category.maLoai}>
-                      {category.tenLoai}
-                    </option>
-                  ))}
-                </select>
                 <td>
                   <button
                     className="edit-button"
@@ -249,6 +233,6 @@ const Foods = () => {
       )}
     </div>
   );
-};
+}
 
 export default Foods;
